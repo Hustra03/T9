026422884f913ba5f8669fa7833556bc05636de7
Hustra03 <erik.paulinder@gmail.com>
2023-10-19 19:11:08 +0200

    modified:   bin/App.class 	modified:   bin/T9.class 	new file:   bin/kelly.txt 	modified:   src/App.java 	modified:   src/T9.java 	new file:   src/kelly.txt

--- src/App.java
+++ src/App.java
@@ -2,9 +2,10 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        T9NumberTest();
-        //T9Test();
-        //test();
+        t9ActualTest();
+        // T9NumberTest();
+        // T9Test();
+        // test();
     }
 
     public static void test() {
@@ -12,10 +13,33 @@ public class App {
         while (toAdd.length() > 0) {
             System.out.println(toAdd);
             String nextString = toAdd.substring(1);
-            char currentLetter = toAdd.charAt(0);
-            toAdd=nextString;
+            toAdd = nextString;
         }
 
+    }
+
+    public static void t9ActualTest() {
+
+        T9 t9 = new T9();
+        t9.T9AddEveryWord();
+        String testString = "uniform";
+
+        List list = t9.returnValidStringForInput(StringToCode(testString));
+        for (Object object : list) {
+            System.out.println("Valid " + object);
+        }
+    }
+
+    public static String StringToCode(String string)
+    {
+
+        char testStringChar[] = string.toCharArray();
+        String testCode = "";
+        for (char c : testStringChar) {
+
+            testCode += charToCode(c);
+        }
+        return testCode;
     }
 
     public static void T9NumberTest() {
@@ -44,5 +68,65 @@ public class App {
         for (Object object : list) {
             System.out.println("Valid " + object);
         }
+    }
+
+    private static int charToCode(char w) {
+        switch (w) {
+            case 'a':
+                return 0;
+            case 'b':
+                return 0;
+            case 'c':
+                return 0;
+            case 'd':
+                return 1;
+            case 'e':
+                return 1;
+            case 'f':
+                return 1;
+            case 'g':
+                return 2;
+            case 'h':
+                return 2;
+            case 'i':
+                return 2;
+            case 'j':
+                return 3;
+            case 'k':
+                return 3;
+            case 'l':
+                return 3;
+            case 'm':
+                return 4;
+            case 'n':
+                return 4;
+            case 'o':
+                return 4;
+            case 'p':
+                return 5;
+            case 'r':
+                return 5;
+            case 's':
+                return 5;
+            case 't':
+                return 6;
+            case 'u':
+                return 6;
+            case 'v':
+                return 6;
+            case 'x':
+                return 7;
+            case 'y':
+                return 7;
+            case 'z':
+                return 7;
+            case 'å':
+                return 8;
+            case 'ä':
+                return 8;
+            case 'ö':
+                return 8;
+        }
+        return -1;
     }
 }
--- src/T9.java
+++ src/T9.java
@@ -1,5 +1,9 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class T9 {
     Node root;
@@ -8,9 +12,22 @@ public class T9 {
         this.root = new Node();
     }
 
-    public void T9AddEveryWord()
-    {
+    public void T9AddEveryWord() {
+        String file = "src\\kelly.txt";
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader((new InputStreamReader(new FileInputStream(file), "UTF-8")));
+            String line = reader.readLine();
 
+            while (line != null) {
+                root.addString(line);
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addString(String toAdd) {
@@ -18,20 +35,18 @@ public class T9 {
         root.addString(toAdd);
 
     }
-    
 
     public List<String> returnValidString(int desiredLength) {
         List<String> list = new LinkedList<String>();
 
-        list= root.returnValidString(list, "",3,0);
+        list = root.returnValidString(list, "", 3, 0);
         return list;
     }
 
-    public List<String> returnValidStringForInput(String code)
-    {
+    public List<String> returnValidStringForInput(String code) {
         List<String> list = new LinkedList<String>();
 
-        list= root.returnValidStringSpecificValue(list,code, "",code.length(),0);
+        list = root.returnValidStringSpecificValue(list, code, "", code.length(), 0);
         return list;
     }
 }
